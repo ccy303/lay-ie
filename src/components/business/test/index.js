bb661f127eaea3eb2d032492e2d055847037fdea
陈超寅 <chaoyinchen@caih.com>
2022-09-20 15:58:15 +0800

    dev

---
+++ src/components/business/test/index.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { observable } from "mobx";
+import { Observer } from "mobx-react-lite";
+import Home from "../home";
+const store = observable({
+    C: null
+});
+export default props => {
+    useEffect(() => {
+        (async () => {
+            const Com = (await import("../home")).default;
+            console.log(Com);
+            store.C = Com;
+            // console.log(123, C);
+            // serCom(C);
+        })();
+    }, []);
+    return (
+        <Observer>
+            {() => {
+                return <>{store.C && <store.C gStore={{}} />}</>;
+            }}
+        </Observer>
+    );
+};
